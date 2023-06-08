330e53caeb3216b4961a7c2ff54e14a85c3d773b
Everton <vatoencube@gmail.com>
2023-06-08 22:06:17 +0200

    Travels endpoint functional

---
+++ app/Http/Controllers/Api/V1/TravelController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Travel;
+use Illuminate\Http\Request;
+use App\Http\Resources\TravelResource;
+
+class TravelController extends Controller
+{
+    public function index()
+    {
+        $travels = Travel::where('is_public',false)->paginate();
+
+        return TravelResource::collection($travels);
+    }
+}
