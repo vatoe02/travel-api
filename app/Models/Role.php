e46b0777ca4d3eb65eb44ff78c714b05a9923719
Everton <vatoencube@gmail.com>
2023-06-08 20:42:21 +0200

    Database schema designed

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,9 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class Role extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
     protected $fillable =['name'];
 }
--- app/Models/Travel.php
+++ app/Models/Travel.php
@@ -3,12 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class Travel extends Model
 {
-    use HasFactory, Sluggable;
+    use HasFactory, Sluggable, HasUuids;
 
     protected $table = 'travels';
 
@@ -20,6 +23,11 @@ class Travel extends Model
         'number_of_days',
     ];
 
+    public function tours():HasMany
+    {
+        return $this->hasMany(Tour::class);
+    }
+
     public function sluggable(): array
     {
         return[
@@ -27,5 +35,10 @@ class Travel extends Model
                 'source' => 'name'
             ]
             ];
+    }
+
+    public function getNumberOfNightsAttribute()
+    {
+        return $this->number_of_days - 1;
     }
 }
